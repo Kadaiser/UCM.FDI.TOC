cf9fce6753ff2715dfbf5f1e57d0f537c1100b39
Diego Valbuena Pineda <dvalbuen@ucm.es>
2015-10-14 16:28:24 +0200

    Corrección del profesor
    Incluidos los comentarios del profesor sobre la practica 1

--- P1b/contador.vhd
+++ P1b/contador.vhd
@@ -37,15 +37,18 @@ architecture Behavioral of contador is
 			C: out STD_LOGIC_VECTOR (3 downto 0));
 	end component;
 
-	signal A: STD_LOGIC_VECTOR (3 downto 0); 
-	signal Ssumador: STD_LOGIC_VECTOR (3 downto 0);
-	signal C: STD_LOGIC_VECTOR (3 downto 0);
+	--signal A: STD_LOGIC_VECTOR (3 downto 0);
+	--signal A: STD_LOGIC_VECTOR (3 downto 0) <= "0001";
+	signal reg_out: STD_LOGIC_VECTOR (3 downto 0);
+	signal reg_in: STD_LOGIC_VECTOR (3 downto 0);
 	
 begin
-	A <= "0001";
-	mod_reg_paralelo: reg_paralelo port map(rst, clk_100mhz , load, C, Ssumador);
-	mod_sumador: sumador port map (A, Ssumador ,C);
-	Z <= Ssumador;
+	--A <= "0001";
+	mod_reg_paralelo: reg_paralelo port map(rst, clk_100mhz , load, reg_in, reg_out);
+	--mod_sumador: sumador port map (A<="0001", reg_out,reg_in);
+	--mod_sumador: sumador port map (A, reg_out ,reg_in);
+	mod_sumador: sumador port map ("0001", reg_out, reg_in);
+	Z <= reg_out;
 
 
 end Behavioral;
