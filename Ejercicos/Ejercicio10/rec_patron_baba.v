f27ebe3894b72eb8d9fc70ebee394a023ebc3b82
Diego Valbuena Pineda <dvalbuen@ucm.es>
2015-11-10 11:49:24 +0100

    Ejercicio 10

---
+++ Ejercicos/Ejercicio10/rec_patron_baba.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:06:51 10/21/2015 
+// Design Name: 
+// Module Name:    rec_patron_baba 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module rec_patron_baba(
+    input entrada STD_LOGIC;
+    output salida STD_LOGIC
+    );
+
+
+endmodule
